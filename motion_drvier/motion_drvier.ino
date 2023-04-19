1ff7225576f60b809465c880b48cbbd8986ab6dd
puch66 <deluca.valerio1999@gmail.com>
2023-04-19 15:38:17 +0200

    fixes, added function for sequential movements

--- motion_drvier/motion_drvier.ino
+++ motion_drvier/motion_drvier.ino
@@ -15,28 +15,36 @@ const int S7_OPEN=130,  S7_CLOSE = 200;
 
 Adafruit_PWMServoDriver controller = Adafruit_PWMServoDriver(0x40);
 
+struct Transition {
+  int fin_pos;
+  float velocity; 
+};
+
 class movement{
 private:
   byte servo;
   int fin_pos;
-  byte velocity;  // min = 0, max = 100
-  bool motion_in_progress = false;
+  float velocity;  // min = 0, max = 1
+  bool motion_in_progress;
   long int t0;
-  byte delay_t0 = 10;  //delay_t0[milliseconds]
-  byte epsilon = 10;
-
-public:
+  byte delay_t0;  //delay_t0[milliseconds]
+  byte epsilon;
   float servo_pos;
   bool executed;
   byte iter;
+
+public:
 
   movement() {
 
   }
 
   movement(byte servo){
-      this-> iter = 0;
-      this -> executed = false;
+      motion_in_progress = false;
+      delay_t0 = 10;
+      epsilon = 10;
+      iter = 0;
+      executed = false;
       this->servo = servo;
       if(servo == 0) this->servo_pos = S0_CLOSE;
       else if(servo == 1) this->servo_pos = S1_CLOSE;
@@ -50,13 +58,12 @@ public:
       controller.setPWM(servo, 0, servo_pos);
   }
     
-  bool new_position(int init_pos,int fin_pos, float velocity){ // min = 0 max = 1
+  bool new_position(Transition t){ // min = 0 max = 1
     if(!motion_in_progress){
-      servo_pos = init_pos;
-      this->fin_pos = fin_pos;
-      this->velocity = velocity;
-      if(velocity<0) this->velocity = 0;
-      else if(velocity>100) this->velocity = 100;
+      fin_pos = t.fin_pos;
+      velocity = t.velocity;
+      if(velocity<0) velocity = 0;
+      else if(velocity>1) velocity = 1;
       motion_in_progress = true;
       t0 = millis();
       return false;
@@ -66,7 +73,6 @@ public:
       if(millis()-t0>delay_t0){
         servo_pos =  (1-velocity)*servo_pos + velocity*fin_pos;
         controller.setPWM(servo, 0, int(servo_pos));
-        delay(10);
       }
       if(servo_pos > fin_pos-epsilon && servo_pos < fin_pos+epsilon){
         servo_pos = fin_pos;
@@ -77,9 +83,61 @@ public:
       else return false;
     }
   }
+
+  bool sequence_of_moves(Transition transitions[], int i) {
+    if(!executed && iter < i) {
+      executed = new_position(transitions[iter]);
+      return false;
+    }
+    else if(iter < i) {
+      iter++;
+      executed = !executed;
+      Serial.print("Transition ");
+      Serial.print(iter);
+      Serial.print(" completed, servo ");
+      Serial.print(servo);
+      Serial.print(" at pos: ");
+      Serial.println(servo_pos);
+      delay(2000);
+      return false;
+    }
+    else {
+      iter = 0;
+      executed = false;
+      return true;
+    }
+  }
+
+  //GETTERS AND SETTERS
+  bool get_executed() {
+    return executed;
+  }
+
+  void set_executed(bool executed) {
+    this->executed = executed;
+  }
+
+  float get_servo_pos() {
+    return servo_pos;
+  }
+
+  void set_servo_pos(float servo_pos) {
+    this->servo_pos = servo_pos;
+  }
+
+  byte get_iter() {
+    return iter;
+  }
+
+  void set_iter(byte iter) {
+    this->iter = iter;
+  }
 };
 
 movement s[16];
+Transition t0[2] = {{S0_OPEN, 0.05}, {S0_CLOSE, 0.05}};
+Transition t1[4] = {{S1_OPEN, 0.05}, {S1_CLOSE, 0.05}, {S1_OPEN, 0.05}, {S1_CLOSE, 0.05}};
+bool done[16];
 
 void setup(){
   Serial.begin(115200);
@@ -91,10 +149,13 @@ void setup(){
   
   for(byte servo=0;servo<6;servo++){
     s[servo] = movement(servo);
+    done[servo] = false;
   }
   Serial.println("Setup finished");
   delay(1000);
 }
+
+
 void loop(){
   /*next_stato = getstatus();
   
@@ -113,18 +174,30 @@ void loop(){
       if(iteration0 == 3 && iteration1 == 4 ...) stato = next_stato;
     }
   }*/
-    if(!s[0].executed && s[0].iter < 2) {
-      if(s[0].iter == 0) s[0].executed = s[0].new_position(s[0].servo_pos, S0_OPEN, 0.05);
-      else if(s[0].iter == 1) s[0].executed = s[0].new_position(s[0].servo_pos, S0_CLOSE, 0.05);
-    }
-    else if(s[0].iter < 2) {
-      s[0].iter ++;
-      s[0].executed = !s[0].executed;
-    }
-    
-    if(s[0].iter == 2) {
-      s[0].iter = 0;
-      s[0].executed = false;
-    }
-    
+  /*if(!s[0].get_executed() && s[0].get_iter() < 2) {
+    if(s[0].get_iter() == 0) s[0].set_executed(s[0].new_position(s[0].servo_pos, S0_OPEN, 0.05));
+    else if(s[0].get_iter() == 1) s[0].set_executed(s[0].new_position(s[0].servo_pos, S0_CLOSE, 0.05));
+  }
+  else if(s[0].get_iter() < 2) {
+    s[0].set_iter(s[0].get_iter() + 1);
+    s[0].set_executed(!s[0].get_executed());
+  }
+  
+  if(s[0].get_iter() == 2) {
+    s[0].set_iter(0);
+    s[0].set_executed(false);
+  }*/
+
+ if(!done[0]) done[0] = s[0].sequence_of_moves(t0, 2);
+ if(!done[1]) done[1] = s[1].sequence_of_moves(t1, 4);
+ if(done[0] && done[1]) {
+   done[0] = false;
+   done[1] = false;
+   Serial.println("SEQUENCE COMPLETED. RESTARTING");
+ }
+
 }
+
+
+
+
